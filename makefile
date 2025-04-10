40aaddad3a2f7de85313f2993db78e29f4283ae2
ralec24 <a24110274@live.ceti.mx>
2025-04-10 05:09:58 +0000

    Practica #5

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-PROYECTO =  memoria
+PROYECTO =  main
 LIB := -lftxui-component -lftxui-dom -lftxui-screen
 CXX := -std=c++17
 SRC := src
@@ -6,12 +6,32 @@ INCLUDE := include
 BIN := bin
 EXE := $(BIN)/$(PROYECTO)
 
-$(EXE) : $(SRC)/memoria.cpp
+$(EXE) : $(SRC)/main.cpp
 	c++ $< -o $@ $(LIB) $(CXX) -I$(INCLUDE)
 
 run : $(EXE)
 	./$<
 
-clean :
-	rm $(BIN)/*
+clean:
+	rm$(BIN)/*
+
+#Practicas
+PRACTICAS = ejecutable
+EXE2 := $(BIN)/$(PRACTICAS)
+practicas: bin/ejecutable
+	./$<
+
+$(EXE2) : $(SRC)/memoria.cpp #nombre archivo
+	c++ $< -o $@ $(LIB) $(CXX) -I$(INCLUDE)
+bin/ejecutable: src/memoria.cpp
+	c++ $< -o $@ -I$(INCLUDE)
+
+run2: $(EXE2)
+archivo: bin/archivo
+	./$<
+
+clean2 :
+	rm $(BIN)/*
+bin/archivo: src/archivo.cpp
+	c++ $< -o $@ -I$(INCLUDE)
 
---
+++ src/archivo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>// se encarga de hacer operaciones con areglos de caracteres
+#include <istream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    //abrir el archivo de escritura
+    ofstream archivo("archivo.txt");
+    if(!archivo.is_open()){
+        cerr << "Error al Abrir el Archivo de Escritura." << endl;
+        return 1;
+    }
+    cout << "Escribiendo en el Archivo...\n" << endl;
+    archivo << "HOLA MUNDO" << endl;
+    for(int i = 0; i < 6; i++){
+        archivo << "Linea " << i+1 << endl;
+    }
+    
+    //abrir el archivo
+    ifstream archivoLectura("archivo.txt");
+    
+    if(!archivoLectura.is_open()){
+        cerr << "Error al Abrir el Archivo de Escritura." << endl;
+        return 1;
+    }
+    cout << "Leyendo del Archivo...\n" << endl;
+    string linea;
+    while(getline(archivoLectura, linea)){
+        cout << linea << endl;
+    }
+    //cerrar el archivo
+    archivo.close();
+    return0;
+}
